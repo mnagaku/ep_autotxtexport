fecabcf9f482cb127f12f5806e553d53e248881b
Masaru Nagaku <mnagaku@gmail.com>
2018-08-11 12:52:57 +0900

    add mkdir

--- ep_autotxtexport.js
+++ ep_autotxtexport.js
@@ -1,7 +1,16 @@
-var getPadPlainText = require('./ExportHelper').getPadPlainText;
+var txtexportdir = "txtexport";
+
+var getPadPlainText = require("../../src/node/utils/ExportHelper").getPadPlainText;
 var fs = require("fs");
+
+exports.mkdirtxtexport = function(hook_name, args, cb) {
+    var s = fs.statSync(txtexportdir);
+    if(s == null) {
+        fs.mkdirSync(txtexportdir);
+    }
+}
 
 exports.autotxtexport = function(hook_name, args, cb) {
     var txt = getPadPlainText(args.pad);
-    fs.writeFile('out.txt', txt);
+    fs.writeFileSync(txtexportdir + "/" + args.pad.id + ".txt", txt);
 }
